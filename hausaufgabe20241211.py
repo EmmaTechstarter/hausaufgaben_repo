fb1ec43398049a90919aa14b700d886b1c6a143a
EmmaTechstarter <emma.feck@tn.techstarter.de>
2024-12-11 23:31:20 +0100

    Hausaufgabe von 11-12-2024 hichgeladen

---
+++ hausaufgabe20241211.py
@@ -0,0 +1,22 @@
+a = 10
+b = 7
+
+
+print("Zahl 1 * Zahl 2=", a * b)
+print("Zahl 1 / Zahl 2=", a / b)
+print("Zahl 1 + Zahl 2=", a + b)
+print("Zahl 1 - Zahl 2=", a - b)
+
+note1 = 100
+note2 = 95
+note3 = 100
+
+print(note1)
+print(note2)
+print(note3)
+
+print("Durchschnitterrechnung", note1 + note2 + note3)
+
+Addierung = 295
+
+print("Durchschnitterrechnung", Addierung / 3)
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Hausaufgaben + Aufgaben
+
+Hier werde ich nur meine Aufgaben und Hausaufgaeb, sowie Tests posten
